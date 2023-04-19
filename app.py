c531693e192aa1afba4889f614669fdc5c82ffe3
chloekim917 <chloekim917@gmail.com>
2023-04-19 17:16:37 +0900

    computer chooses rock paper scissor and display

--- app.py
+++ app.py
@@ -3,17 +3,27 @@ while True:
     typed = input("Type 'rock' or 'paper' or 'scissors': ")
     if typed.upper() == 'ROCK':
         rock = typed.upper()
-        #print(rock)
+        print(f"User: {rock}")
         break
     elif typed.upper() == 'PAPER':
         paper = typed.upper()
-        #print(paper)
+        print(f"User: {paper}")
         break
     elif typed.upper() == 'SCISSORS':
         scissors = typed.upper()
-        #print(scissors)
+        print(f"User: {scissors}")
         break
     else:
         print("Let's try this again")
 
-
+import random
+comp = random.randint(1,3)
+if comp == 1:
+    comp = "ROCK"
+    print(f"Computer: {comp}")
+elif comp == 2:
+    comp = "PAPER"
+    print(f"Computer: {comp}")
+else:
+    comp = "SCISSORS"
+    print(f"Computer: {comp}")
