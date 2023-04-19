83979cfc54f5e58b42d60b6dfaf1b2238aa8236d
chloekim917 <chloekim917@gmail.com>
2023-04-19 17:45:39 +0900

    code works as intended

--- app.py
+++ app.py
@@ -13,18 +13,36 @@ while True:
     typed = input("Type 'rock' or 'paper' or 'scissors': ")
     if typed.upper() == 'ROCK':
         user = 'ROCK'
-        print(f"Computer: {comp}")
         print(f"User: {user}")
+        print(f"Computer: {comp}")
+        if comp == 'ROCK':
+            print("It's a tie! Nice try")
+        elif comp == "PAPER":
+            print("Haha you lose! I am a superior being")
+        else: 
+            print("I cannot believe this... How dare you human... You win...")
         break
     elif typed.upper() == 'PAPER':
         user = 'PAPER'
-        print(f"Computer: {comp}")
         print(f"User: {user}")
+        print(f"Computer: {comp}")
+        if comp == 'ROCK':
+            print("I cannot believe this... How dare you human... You win...")
+        elif comp == "PAPER":
+            print("It's a tie! Nice try")
+        else: 
+            print("Haha you lose! I am a superior being")
         break
     elif typed.upper() == 'SCISSORS':
         user = 'SCISSORS'
-        print(f"Computer: {comp}")
         print(f"User: {user}")
+        print(f"Computer: {comp}")
+        if comp == 'ROCK':
+            print("Haha you lose! I am a superior being")
+        elif comp == "PAPER":
+            print("I cannot believe this... How dare you human... You win...")
+        else: 
+            print("It's a tie! Nice try")
         break
     else:
         print("Let's try this again")
