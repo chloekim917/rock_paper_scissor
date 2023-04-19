356fa80ef8eab60a96736058a75695e074bc7b55
chloekim917 <chloekim917@gmail.com>
2023-04-19 17:33:00 +0900

    changed variables to user

--- app.py
+++ app.py
@@ -1,31 +1,30 @@
 print("Let's play the game of rock, paper, scissors.")
-while True:
-    typed = input("Type 'rock' or 'paper' or 'scissors': ")
-    if typed.upper() == 'ROCK':
-        rock = typed.upper()
-        print(f"User: {rock}")
-        break
-    elif typed.upper() == 'PAPER':
-        paper = typed.upper()
-        print(f"User: {paper}")
-        break
-    elif typed.upper() == 'SCISSORS':
-        scissors = typed.upper()
-        print(f"User: {scissors}")
-        break
-    else:
-        print("Let's try this again")
 
 import random
 comp = random.randint(1,3)
 if comp == 1:
     comp = "ROCK"
-    print(f"Computer: {comp}")
 elif comp == 2:
     comp = "PAPER"
-    print(f"Computer: {comp}")
 else:
     comp = "SCISSORS"
-    print(f"Computer: {comp}")
 
-if 
+while True:
+    typed = input("Type 'rock' or 'paper' or 'scissors': ")
+    if typed.upper() == 'ROCK':
+        user = 'ROCK'
+        print(f"Computer: {comp}")
+        print(f"User: {user}")
+        break
+    elif typed.upper() == 'PAPER':
+        user = 'PAPER'
+        print(f"Computer: {comp}")
+        print(f"User: {user}")
+        break
+    elif typed.upper() == 'SCISSORS':
+        user = 'SCISSORS'
+        print(f"Computer: {comp}")
+        print(f"User: {user}")
+        break
+    else:
+        print("Let's try this again")
