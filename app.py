91d6c5ce6cff242c31df604b24d1aefceb552020
chloekim917 <chloekim917@gmail.com>
2023-04-19 16:55:03 +0900

    input variable assignment done

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 print("Let's play the game of rock, paper, scissors.")
 while True:
     typed = input("Type 'rock' or 'paper' or 'scissors': ")
-    if typed.upper() == 'ROCK' or typed.upper() == 'PAPER' or typed.upper() == 'SCISSORS':
-        #print('well done')
-        #import random
-
+    if typed.upper() == 'ROCK':
+        rock = typed.upper()
+        #print(rock)
+        break
+    elif typed.upper() == 'PAPER':
+        paper = typed.upper()
+        #print(paper)
+        break
+    elif typed.upper() == 'SCISSORS':
+        scissors = typed.upper()
+        #print(scissors)
         break
     else:
-        print("Let's try this again.")
+        print("Let's try this again")
+
 
